d712013f6f12398430c0a6dbdf2b04054bf194b0
nn70 <nn4work@gmail.com>
2026-01-08 00:05:21 +0800

    Add probe routes for debugging

---
+++ app/test-deployment/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div style={{ padding: '50px', color: 'white' }}>
+            <h1>Test Page Deployment OK</h1>
+            <p>If you can see this, the Next.js server is rendering pages correctly.</p>
+        </div>
+    )
+}
